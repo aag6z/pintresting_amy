280a3d0bca24e9a9882c0529b170f9ed6e84d669
AmyG <amruta.ghate@gmail.com>
2014-11-22 11:11:35 -0500

    nov22

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # Fix this
-  config.action_mailer.default_url_options = { :host => 'http://pinteresting-commits.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://limitless-plains-8129.herokuapp.com/' }
 end
