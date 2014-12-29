f545b7fb007edc7d4285c0ac1773ce8191ceee3b
AmyG <amruta.ghate@gmail.com>
2014-12-28 20:24:43 -0500

    comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_attached_file :avatar, 
-    :styles => { :medium => "300x300>", :thumb => "100x100>" }  
+    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :url => ":id/:style/:basename.:extension",
+    :path => ":rails_root/app/assets/images/:id/:style/:basename.:extension",
+    :default_url => "missing.jpg", 
+    :use_timestamp => false
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/      
   validates :name, presence: true
   has_many :pins
 end
 
 
-#:url => ":id/:style/:basename.:extension",
-#:path => ":rails_root/app/assets/images/:id/:style/:basename.:extension",
-#:default_url => "missing.jpg",
-#:use_timestamp => false
+
+
